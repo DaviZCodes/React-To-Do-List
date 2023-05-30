6839221c249f0279b5f61b4f5bf9d7400386fbb8
davizinho <davi.bestguy@gmail.com>
2023-05-30 18:36:30 -0400

    e

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 
+function addTodo(input_text){
+  
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <header> Write your to-do here. </header>
+        <div class = "Add-To-Do">
+          <input type = "text" id = "input"></input>
+          <button id = "button" onClick = {addTodo}> Add To-Do </button>
+        </div>
       </header>
     </div>
   );
