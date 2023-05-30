02419487ff35bc9a71fd973d9fdc0ee2bec1ce6b
davizinho <davi.bestguy@gmail.com>
2023-05-30 18:46:22 -0400

    added some things

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function addTodo(input_text){
-  
+
 }
 
 function App() {
+  //use State hook
+  const [toDo, settoDo] = useState("");
+
   return (
     <div className="App">
       <header className="App-header">
-        <header> Write your to-do here. </header>
-        <div class = "Add-To-Do">
-          <input type = "text" id = "input"></input>
+        <header id = "head"> Write your to-dos here. </header>
+
+        <div class = "Add-To-Do"> 
+          <input type = "text" id = "input" placeholder="Write your to-do..." 
+            value = {toDo} onChange = {item => settoDo(item.target.value)}></input>
           <button id = "button" onClick = {addTodo}> Add To-Do </button>
         </div>
       </header>
