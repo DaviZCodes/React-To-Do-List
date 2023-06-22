e5858dafa7a86693d73942a2cc246ecdef292650
DaviZCodes <davi.bestguy@gmail.com>
2023-06-22 14:41:18 -0400

    changed server js to tsx

---
+++ backend/server.tsx
@@ -0,0 +1,23 @@
+//backend for API calls to resolve CORS issue
+
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+app.get("/quotes", (req: any, res: { json: (arg0: { quote: any; }) => void; status: (arg0: number) => { (): any; new(): any; json: { (arg0: { error: string; }): void; new(): any; }; }; }) => {
+  axios.get("https://zenquotes.io/api/random")
+    .then((response: { data: any; }) => {
+      const data = response.data;
+      res.json({ quote: data[0].q });
+    })
+
+    .catch((error: any) => {
+      console.error("API could not fetch", error);
+      res.status(500).json({ error: "Quote could not be acquired from the API."});
+    });
+});
+
+app.listen(3001);
